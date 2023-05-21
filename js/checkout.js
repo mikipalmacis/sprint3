2be3dd45f112553d5a4e1ef4e78f47ebeb571e0b
Miguel A. Palma <65388599+mikipalm@users.noreply.github.com>
2023-05-21 18:25:49 +0200

    Modificaciones mejoras Ejercicio 7

--- js/checkout.js
+++ js/checkout.js
@@ -1,28 +1,20 @@
 
 // Exercise 6
-function validate() {
-	var error = 0;
-	// Get the input fields
-	var fName = document.getElementById("fName");
-	var fEmail = document.getElementById("fEmail");
-
-	// Get the error elements
-	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");  
-	
-	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ""){
-		error++;
-	}
-
-	if(fEmail.value == ""){
-		error++;
-	}
-	 
-	if(error>0){
-		alert("Error");
-	}else{
-		alert("OK");
-	}
-
-}
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+	'use strict';
+  
+	// Fetch all the forms we want to apply custom Bootstrap validation styles to
+	const forms = document.querySelectorAll('.needs-validation');
+  
+	// Loop over them and prevent submission
+	Array.prototype.slice.call(forms).forEach((form) => {
+	  form.addEventListener('submit', (event) => {
+		if (!form.checkValidity()) {
+		  event.preventDefault();
+		  event.stopPropagation();
+		}
+		form.classList.add('was-validated');
+	  }, false);
+	});
+  })();
